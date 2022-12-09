8aaf4336fc478b787f463996d2f0787fa47246da
ArjavaTripathi <arjavatripathi21@singhaniaschools.org>
2022-12-09 16:28:56 +0530

    Co-authored-by: Stockrainn <Stockrainn@users.noreply.github.com> Co-authored-by: Arnav-pm <Arnav-pm@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
-const functions = require("firebase-functions");
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = fucntions.https.onRequest((req,res)  => {
+    Investor = req.params.name
+    CompanyStock = req.params.company
+
+})
