36d941d3a1944a7ec4df171335911812c03521ef
ArjavaTripathi <100183634+ArjavaTripathi@users.noreply.github.com>
2022-12-11 00:53:09 +0530

    Added firebase (working)

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/app";
+import "firebase/firestore"; // Import the Firestore service
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,6 +14,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase(firebaseConfig);
 const db = app.firestore();
+
+// Export the Firebase app instance and the Firestore database instance
 export { db };
