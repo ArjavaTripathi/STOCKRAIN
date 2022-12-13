614cc187673e189f434aa615baeeb1b769d733a3
ArjavaTripathi <arjavatripathi21@singhaniaschools.org>
2022-12-13 21:17:52 +0530

    updated

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const async = require('async');
 
 
 admin.initializeApp();  //Create function to figure out which time to use! 
 
+
+
+
+
+
+
+
+async function getTimePeriod(pricePeriodRef) {
+  let time_period;
+
+  try {
+    await pricePeriodRef.get().then(doc => {
+      if (doc.exists) {
+        time_period = doc.TimePeriod;
+      }
+    });
+
+    return time_period;
+  } catch (error) {
+    return false;
+  }
+}
 
 
 exports.buy = functions.https.onRequest((req, res) => {
@@ -12,32 +35,28 @@ exports.buy = functions.https.onRequest((req, res) => {
   const industry = query.industry;
   var company = query.company; 
   var nStocks = query.stocks;
-  var price = 0;
   const stocksRef = admin.firestore().collection("Stocks");
   const industryRef = stocksRef.collection(industry);
   const companyref = IndustryRef.collection(company);
-  const StockRate = companyref = companyref.doc("StockRates");
-  let time_period = 0;
+  const StockRate = companyref.doc("StockRates");
   const pricePeriodRef = firestore.collection("Stocks").doc("PricePeriod");
   var balance, freeStocks, price;
 
-  
-  pricePeriodRef.get().then(doc => {     //Reads what time period it is 
-    if (doc.exists) {
-      time_period = doc.TimePeriod
-    }
+
+  time = getTimePeriod();
+
+  if(typeof time == "number"){
+    StockRate.get("rates").then(doc => {  //gets rates of the chosen company if time value has no error
+      if (doc.exists) {
+        price = rates[time];
+      }
     });
+  } else{
+    res.status(404).set("Database Error").send("Could not find time period in database!");
+    return;
+  }
 
-  
-  
 
- 
-
-  StockRate.get("rates").then(doc => {  //gets rates of the chosen company 
-    if (doc.exists) {
-      price = rates[time_period];
-    }
-  });
 
   if(balance>=nStocks*price && nStocks<=freeStocks){    //Updates stocks/balance of the investor
     balance-=nStocks*price;
@@ -48,26 +67,34 @@ exports.buy = functions.https.onRequest((req, res) => {
       freeStocks: freeStocks,
       balance: balance
     });
-    res.json({ success: true });
+
+
+    response.status(202).send("Updated Successfully");
   } else{
-    res.json({ success: false });
+
+
+    response.status(500).set("Internal Server Error").send("Investors balance could not be updated");
+    return;
+
   }
 });
 
-exports.sell = functions.https.onRequest((req, res) => {
-    var clientIp = req.headers['x-forwarded-for'];
-    let company = req.query.company;
-    let stocks = req.query.stocks;
+exports.sell = functions.https.onRequest((request, response) => {
+    var clientIp = request.headers['x-forwarded-for'];
+    let company = request.query.company;
+    let stocks = request.query.stocks;
     if (ownedStocks >= nStocks) {
 
 
       balance += (stocks *price);//reflects change in money after selling stuff
       //freeStocks += nStocks;//checks if the dude got enough stocks to sell
       //ownedStocks -= nStocks;//updates owned stocks
-      return true;
+
+      response.status(200).send("Success!");
     }
       else {
-      return false;
+      response.status(500).send("Failure! Updating balance could not be resolved.");
+      return;
   }
   
 
@@ -77,19 +104,19 @@ exports.sell = functions.https.onRequest((req, res) => {
   
 
 
-exports.profile = functions.https.onRequest((req, res) => {
-  clientIp = req.headers['x-forwarded-for']
+exports.profile = functions.https.onRequest((request, response) => {
+  clientIp = request.headers['x-forwarded-for']
   //cannot work on this till profile dabase is made. This will get the values of all stocks and from which companies the guy owns.
 
 })
 
-exports.market = functions.https.onRequest((res, req) => {
-  var clientIp = req.headers['x-forwarded=for']
-  let Industry = req.query.industry
+exports.market = functions.https.onRequest((response, request) => {
+  var clientIp = request.headers['x-forwarded=for']
+  let Industry = request.query.industry
   
 })
 
-exports.changeprice = functions.https.onRequest((res, req)=> {
+exports.changeprice = functions.https.onRequest((response, request)=> {
 
   const pricePeriodRef = firestore.collection("Stocks").doc("PricePeriod");
 
@@ -99,3 +126,9 @@ exports.changeprice = functions.https.onRequest((res, req)=> {
   
 
 })
+
+exports.test = functions.https.onRequest((request, response) => {
+  price = 69;
+  response.send(price)
+});
+
