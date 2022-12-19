6ee11c960be2d40f4f6df3d8a46fcd7c2c5923c4
ArjavaTripathi <100183634+ArjavaTripathi@users.noreply.github.com>
2022-12-19 20:18:07 +0530

    WORKING?

--- functions/index.js
+++ functions/index.js
@@ -15,65 +15,8 @@ AWS.config.update({
 
 const db = new AWS.DynamoDB.DocumentClient();
 
-exports.signup = functions.https.onRequest(async (request, response) => {
- // Get the attribute value from the request query parameters
- const email = "arjavatripathi5@gmail.com";
 
- // Create a new instance of the DynamoDB.DocumentClient class
- const docClient = new AWS.DynamoDB.DocumentClient();
-
- // Set the table name and the key condition expression for the query
- const params = {
-   TableName: 'Profile',
-   KeyConditionExpression: '#user = :user ',  // Specify the primary key and sort key attributes and their values in the key condition expression
-   ExpressionAttributeValues: {
-     ':user': email  // Provide the values for the placeholders in the key condition expression
-
-   },
-   ExpressionAttributeNames: {
-    '#user': 'user'  // Map the expression attribute name '#user' to the actual attribute name 'user'
-  }
- };
-
- // Try to query the table
- try {
-   const result = await docClient.query(params).promise();
-
-   // If the query returned any items, return them in the response
-   if (result.Items.length > 0) {
-     response.send(result.Items);
-   } else {
-  // Email was not found in the table, so create a new item
-  const newItemParams = {     //WORKS!!!
-    TableName: 'Profile',
-    Item: {
-      'user': email,
-      'company ': 'tesla'
-    }
-  };
-
-  try {
-    await docClient.put(newItemParams).promise();
-    response.send(true);
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("Error creating new item!")
-  }
-}
- } catch (error) {
-   console.error(error);
-   response.status(500).send("Error!")
-
-}
-
-});
-
-
-
-
-
-
-exports.buy = functions.https.onRequest(async (request, response) => {
+exports.getIndex = functions.https.onRequest(async (request, response) => {
     const Table = 'Profile';
     const readAllUsers = async() => {
         const params = {
@@ -87,45 +30,6 @@ exports.buy = functions.https.onRequest(async (request, response) => {
     }
 }
     const result = await readAllUsers();
-    console.log(result);
-})
-
-exports.sell = functions.https.onRequest(async (request, response) => {
-    
-})
-
-exports.Profile = functions.https.onRequest(async (request, response) => {
-    const Table = 'Profile';
-    const readAllUsers = async() => {
-        const params = {
-            TableName: time
-    };
-    try{
-        const { Items = [] } = await db.scan(params).promise()
-        return{Items}
-    } catch(error){
-        return{data : "Nothing"}
-    }
-}
-    const result = await readAllUsers();
     response.send(result);
-})
-
-
-exports.getIndex = functions.https.onRequest(async (request, response) => {
-    const Table = 'Profile';
-    const readAllUsers = async() => {
-        const params = {
-            TableName: time
-    };
-    try{
-        const { Items = [] } = await db.scan(params).promise()
-        return{Items}
-    } catch(error){
-        return{data : "Nothing"}
-    }
-}
-    const result = await readAllUsers();
-    console.log(result);
 
 })
